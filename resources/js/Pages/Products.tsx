368e957b8ac9fa1c709f5a39ffacd4725827cddb
Phelipe Curty <71573615+curtytech@users.noreply.github.com>
2024-10-25 20:03:46 -0300

    layout and translation of profile

--- resources/js/Pages/Products.tsx
+++ resources/js/Pages/Products.tsx
@@ -1,6 +1,5 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-import { Trash } from "@phosphor-icons/react/dist/ssr";
 import { useState } from 'react';
 
 export default function Products() {
@@ -70,41 +69,42 @@ export default function Products() {
             <div className="mx-[50px] mt-10 bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md w-86">
                 <h1 className="text-2xl font-bold mb-6 text-gray-800 dark:text-white">Cadastrar Produto</h1>
                 <form onSubmit={handleSubmit}>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                    <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+
+                        <div className="mb-4">
+                            <label htmlFor="image" className="block text-sm font-medium text-gray-700 dark:text-gray-300">Imagem</label>
+                            <input type="file" id="image" name="image" accept="image/*" onChange={handleChange} className="bg-gray-600 text-white placeholder:text-gray-300 mt-1 block w-full border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400" />
+                        </div>
+
                         <div className="mb-4">
                             <label htmlFor="barcode" className="block text-sm font-medium text-gray-700 dark:text-gray-300">Código de Barras</label>
-                            <input type="number" id="barcode" name="barcode" required onChange={handleChange} className="mt-1 block w-full border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400" placeholder="Insira o código de barras" />
+                            <input type="number" id="barcode" name="barcode" required onChange={handleChange} className="bg-gray-600 text-white placeholder:text-gray-300 mt-1 block w-full border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400" placeholder="Insira o código de barras" />
                         </div>
 
                         <div className="mb-4">
                             <label htmlFor="name" className="block text-sm font-medium text-gray-700 dark:text-gray-300">Nome</label>
-                            <input type="text" id="name" name="name" required onChange={handleChange} className="mt-1 block w-full border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400" placeholder="Insira o nome do produto" />
+                            <input type="text" id="name" name="name" required onChange={handleChange} className="bg-gray-600 text-white placeholder:text-gray-300 mt-1 block w-full border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400" placeholder="Insira o nome do produto" />
                         </div>
 
                         <div className="mb-4">
                             <label htmlFor="sell_price" className="block text-sm font-medium text-gray-700 dark:text-gray-300">Preço de Venda</label>
-                            <input type="number" id="sell_price" name="sell_price" step="0.01" required onChange={handleChange} className="mt-1 block w-full border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400" placeholder="Insira o preço de venda" />
+                            <input type="number" id="sell_price" name="sell_price" step="0.01" required onChange={handleChange} className="bg-gray-600 text-white placeholder:text-gray-300 mt-1 block w-full border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400" placeholder="Insira o preço de venda" />
                         </div>
 
-                        <div className="mb-4">
-                            <label htmlFor="image" className="block text-sm font-medium text-gray-700 dark:text-gray-300">Imagem</label>
-                            <input type="file" id="image" name="image" accept="image/*" onChange={handleChange} className="mt-1 block w-full border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400" />
-                        </div>
-
-                        <div className="mb-4">
-                            <label htmlFor="description" className="block text-sm font-medium text-gray-700 dark:text-gray-300">Descrição</label>
-                            <textarea id="description" name="description" rows="4" required onChange={handleChange} className="mt-1 block w-full border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400" placeholder="Insira a descrição do produto"></textarea>
-                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="description" className="block text-sm font-medium text-gray-700 dark:text-gray-300">Descrição</label>
+                        <textarea id="description" name="description" rows="3" required onChange={handleChange} className="bg-gray-600 text-white placeholder:text-gray-300 mt-1 block w-full border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400" placeholder="Insira a descrição do produto"></textarea>
                     </div>
 
                     <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 rounded mt-4">Cadastrar Produto</button>
                 </form>
             </div>
 
-            <button onClick={() => {
+            {/* <button onClick={() => {
                 const csrfTokenMeta = document.querySelector('meta[name="csrf-token"]');
                 console.log(csrfTokenMeta ? csrfTokenMeta.content : 'Token CSRF não encontrado!');
-            }}>teste</button>
+            }}>teste</button> */}
 
         </AuthenticatedLayout>
     );
